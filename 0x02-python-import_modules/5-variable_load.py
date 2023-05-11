3d265258b7ca16f07d99a3f568dbaa23c3724029
munenepaul <evansmunenepaul@gmail.com>
2023-05-11 08:18:01 -0700

    5-variable_load.py

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Retrieve and print the value of variable x from variable_module."""
+    from variable_load_5 import x as my_var
+    print(my_var)
