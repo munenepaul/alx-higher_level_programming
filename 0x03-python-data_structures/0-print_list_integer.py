bf18f0e44eeb9b0358ce8eebd5b5dc458f874521
munenepaul <evansmunenepaul@gmail.com>
2023-05-15 05:12:34 -0700

    0-print_list_integer.py update

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
 def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{}".format(num))
+    """Prints all integers of a list without any import module"""
+    for num in range(len(my_list)):
+        print('{:d}'.format(my_list[num]))
