98be49f6b7261ec95cca40fd21b85a5b3855d02d
munenepaul <evansmunenepaul@gmail.com>
2023-05-15 05:49:27 -0700

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" ")
-        print()
+    for row in matrix:
+        row_str = ""
+        for i, num in enumerate(row):
+            if i != 0:
+                row_str += " "
+            row_str += chr(num + ord('0'))
+        print(row_str)
