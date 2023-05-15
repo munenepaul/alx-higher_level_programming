222131e9ddef1939fdea6c93b20353f18d04fefa
munenepaul <evansmunenepaul@gmail.com>
2023-05-15 01:54:32 -0700

    update 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """Retrieve and print the value of ariable x from variable_load_5."""
-    from variable_load_5 import x
-    print(x)
+if __name__ == '__main__':
+    import variable_load_5
+
+    print(variable_load_5.a)
