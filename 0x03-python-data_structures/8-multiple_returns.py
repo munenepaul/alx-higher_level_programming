218c3b9ee91c49af8703ad39683a91e79acfd749
munenepaul <evansmunenepaul@gmail.com>
2023-05-15 06:27:30 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = 0
-    first_char = None
-
-    for char in sentence:
-        length += 1
-        first_char = char
-        break
-
-    return (length, first_char)
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
