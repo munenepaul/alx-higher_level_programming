eb3e4080d44013265df448657753ab30d6388bee
munenepaul <evansmunenepaul@gmail.com>
2023-05-17 10:39:21 -0700

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def update_dictionary(some_dictionary, new_key, new_value):
+def update_dictionary(a_dictionary, key, value):
     """Replaces or adds a key/value pair in a dictionary"""
     if key not in a_dictionary:
         a_dictionary[key] = value
