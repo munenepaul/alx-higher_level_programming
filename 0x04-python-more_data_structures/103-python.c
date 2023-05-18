59c3ddf63abe020ccc41fe9efa880013c1b7307a
munenepaul <evansmunenepaul@gmail.com>
2023-05-18 01:52:45 -0700

    103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -37,6 +37,7 @@ void print_python_list(PyObject *obj)
 	printf("[*] Size of the Python List = %li\n", size);
 	printf("[*] Allocated = %li\n", list->allocated);
 	for (i = 0; i < size; i++)
+
 	{
 		type = (list->ob_item[i])->ob_type->tp_name;
 		printf("Element %i: %s\n", i, type);
