a999f79a0b82b79b0bb3ea93e73c9b40dc642ac8
munenepaul <evansmunenepaul@gmail.com>
2023-05-29 12:10:38 -0700

    task complete

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             tally += 1
-        except (ValueError, TypeError)
+        except (ValueError, TypeError):
             pass
         print()
         return tally
