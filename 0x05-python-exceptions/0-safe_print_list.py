1ca74cd041dea1733b4f323a3c4877f8fa52c8d4
munenepaul <evansmunenepaul@gmail.com>
2023-05-29 11:50:24 -0700

    done

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            try:
+                element = my_list[i]
+                print(element, end=" ")
+            except IndexError:
+                break
+    except:
+        pass
+    print()
+    return min(x, len(my_list))
