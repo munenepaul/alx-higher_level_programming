b5b941b3b7b6f3144726f52fcee8ff3cee64cbd5
munenepaul <evansmunenepaul@gmail.com>
2023-05-30 11:07:41 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+class square:
+    """Initialize a new Square.
+    Args:
+        size (int): The size of the new square.
+        """
+        self.__size = size
