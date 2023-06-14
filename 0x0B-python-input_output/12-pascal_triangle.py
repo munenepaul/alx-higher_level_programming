f6e80f66c33fdfc91d0b2d8b5cabbf0ac9507b47
munenepaul <evansmunenepaul@gmail.com>
2023-06-14 02:36:59 -0700

    update

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a Pascal's Triangle function."""
+
+
+def pascal_triangle(n):
+    """Represent Pascal's Triangle of size n.
+    Returns a list of lists of integers representing the triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i - 1]
+
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+
+        row.append(1)
+        triangle.append(row)
