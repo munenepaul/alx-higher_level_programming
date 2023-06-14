cb794985b6d1da92a8dc6de41daa1c78c33ff8b9
munenepaul <evansmunenepaul@gmail.com>
2023-06-14 02:00:20 -0700

    done

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Contains the read_file function
+"""
+
+
+def write_file(filename="", text=""):
+     """""reads a text file(UTF8) and prints it to stdout"""
+     with open(filename, "w", encoding="utf-8") as file:
+         file.write(text)
+         return len(text)
