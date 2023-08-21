fe6ba36de4eb75529ad48c93800faf9ca839b123
munenepaul <evansmunenepaul@gmail.com>
2023-08-21 02:50:44 -0700

    done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,15 +4,41 @@ python script that lists all states from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
-from sys import argv
+import sys
+
+def list_states(mysql_username, mysql_password, database_name):
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(host="localhost", port=3306,
+                user=mysql_username, passwd=mysql_password, db=database_name)
+        cursor = db.cursor()
+
+        # Execute the query to fetch states and sort by states.id
+        query = "SELECT * FROM states ORDER BY states.id ASC;"
+        cursor.execute(query)
+
+        # Fetch all rows from the result
+        rows = cursor.fetchall()
+
+        # Print the list of states
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
+    finally:
+        # Close the database connection
+        if db:
+            db.close()
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-            passwd=argv[2], db=argv[3], charset="utf8")
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-    cursor.close()
-    db.close()
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <mysql_username>
+        <mysql_password> <database_name>")
+        sys.exit(1)
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    list_states(mysql_username, mysql_password, database_name)
